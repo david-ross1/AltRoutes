56f1c36d11661416b4a4cf276f58519159484121
dross <davidr7711@gmail.com>
2021-06-01 16:35:39 -0700

    Static_pages, entry file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/app",
+  entry: "./frontend/entry_more_trails",
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"

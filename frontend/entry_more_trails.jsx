36d77bd212c68379f034fbd0c346c61c0ac12409
dross <davidr7711@gmail.com>
2021-06-01 20:36:38 -0700

    Cleanup comments and testing

--- frontend/entry_more_trails.jsx
+++ frontend/entry_more_trails.jsx
@@ -3,27 +3,10 @@ import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
 
-//! TESTING
-import { signup, login, logout } from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
-
-  //! TESTING
-  window.store = store;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-
   ReactDOM.render(<Root store={store} />, root);
 });
 
-// let mo = {
-//   fname: 'mo',
-//   lname: 'jo',
-//   email: 'email',
-//   password: 123456
-// }

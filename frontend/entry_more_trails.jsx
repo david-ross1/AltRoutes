5633bb2dba3fc3f4638efdd69b851e50198c70a4
dross <davidr7711@gmail.com>
2021-06-01 18:57:14 -0700

    Add requirements to application.js manifest

--- frontend/entry_more_trails.jsx
+++ frontend/entry_more_trails.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+import configureStore from './store/store'
+import { signup, login, logout } from "./util/session_api_util";
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+
+  const store = configureStore()
+  window.store = store
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.login = login
+  window.signup = signup
+  window.logout = logout
+
   ReactDOM.render(<h1>entry file here</h1>, root);
 });
+
+// let mo = {
+//   fname: 'mo',
+//   lname: 'jo',
+//   email: 'email',
+//   password: 123456
+// }

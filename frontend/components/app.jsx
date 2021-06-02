aef446e2290ea261b324beaafb6cabcc0cd88a2a
dross <davidr7711@gmail.com>
2021-06-01 19:09:10 -0700

    Connect Root & App to entry file

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>app.jsx here</h1>
+  </div>
+);
+
+export default App;
--- frontend/entry_more_trails.jsx
+++ frontend/entry_more_trails.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
-
-import configureStore from './store/store'
+//! TESTING
 import { signup, login, logout } from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
 
-  const store = configureStore()
-  window.store = store
+  //! TESTING
+  window.store = store;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  window.login = login
-  window.signup = signup
-  window.logout = logout
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
 
-  ReactDOM.render(<h1>entry file here</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
 
 // let mo = {

382673b3aafcfb7fc485e61c77d43509a7d91d71
dross <davidr7711@gmail.com>
2021-06-03 10:35:53 -0700

    Add migration create photos table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_172836) do
+ActiveRecord::Schema.define(version: 2021_06_03_173326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(version: 2021_06_03_172836) do
     t.string "park_type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "trail_id", null: false
+    t.string "caption", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trail_id"], name: "index_photos_on_trail_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "trails", force: :cascade do |t|

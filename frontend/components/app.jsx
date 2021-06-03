00f8537d1ade908ec286ffd0c16bccfe19ca2fd6
dross <davidr7711@gmail.com>
2021-06-02 22:17:14 -0700

    Add navbar container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,10 +4,12 @@ import { Route, Link } from 'react-router-dom';
 import LoginFormContainer from '../components/session_form/login_form_container'
 import SignupFormContainer from '../components/session_form/signup_form_container'
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import NavBarContainer from './nav/nav_container';
 
 const App = () => (
   <div>
     <header>
+      <NavBarContainer />
       <h1>More Trails</h1>
       <GreetingContainer />
       <AuthRoute path="/login" component={LoginFormContainer} />

4198087c1def07e86f8f459b1ddc3e1ae6542c7b
dross <davidr7711@gmail.com>
2021-06-03 10:40:23 -0700

    Add migration create reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_173326) do
+ActiveRecord::Schema.define(version: 2021_06_03_173616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,18 @@ ActiveRecord::Schema.define(version: 2021_06_03_173326) do
     t.datetime "updated_at", null: false
     t.index ["trail_id"], name: "index_photos_on_trail_id"
     t.index ["user_id"], name: "index_photos_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "trail_id", null: false
+    t.integer "rating", null: false
+    t.text "text", null: false
+    t.date "date", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trail_id"], name: "index_reviews_on_trail_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "trails", force: :cascade do |t|
